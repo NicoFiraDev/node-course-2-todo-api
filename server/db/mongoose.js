f9883cac6b1fafbb098f46e510c3fedd45585dc7
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2018-12-12 14:28:31 -0500

    Setup heroku db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect('mongodb://nicodev:nicodev123@ds131531.mlab.com:31531/todo-app' || 'mongodb://localhost:27017/TodoApp', {
   useNewUrlParser: true
 });
 
