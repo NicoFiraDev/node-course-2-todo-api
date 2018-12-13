5fb55723d01e28976ad138ffdf4629145be2c039
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2018-12-13 11:38:26 -0500

    Test the DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://nicodev:nicodev123@ds131531.mlab.com:31531/todo-app' || 'mongodb://localhost:27017/TodoApp', {
   useNewUrlParser: true
 });
 
 // MongoDB connection only for localhost
-//mongoose.connect('mongodb://localhost:27017/TodoApp', {
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {
 //   useNewUrlParser: true
 // });
 
