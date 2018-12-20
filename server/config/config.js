2b22a143350b95b41882d133df9db902a0f2723a
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2018-12-19 19:12:36 -0500

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true};
-} else if (env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest', {useNewUrlParser: true};
-} else {
-  process.env.MONGODB_URI = 'mongodb://nicodev:nicodev123@ds131531.mlab.com:31531/todo-app', {useNewUrlParser: true}
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if(env === 'development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true};
+// } else if (env === 'test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest', {useNewUrlParser: true};
+// }
+// else {
+//   process.env.MONGODB_URI = 'mongodb://nicodev:nicodev123@ds131531.mlab.com:31531/todo-app', {useNewUrlParser: true}
+// }
